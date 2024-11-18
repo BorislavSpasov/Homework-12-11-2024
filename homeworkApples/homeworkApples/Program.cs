e22a14c76ff1b411b5408696508281d2354f56ca
BorislavSpasov <borislav.e.spasov@gmail.com>
2024-11-18 19:17:28 +0200

    Add files via upload

---
+++ homeworkApples/homeworkApples/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("box size = ");
+int boxSize = int.Parse(Console.ReadLine());
+
+if (boxSize < 1 || boxSize > 40)
+{
+    return;
+}
+
+int redApples = 0, yellowApples = 0;
+
+for (int i = 1; i <= boxSize; i++)
+{
+    if (i % 2 == 0)
+    {
+        redApples += i * i;
+    }
+    else
+    {
+        yellowApples += i * i;
+    }
+}
+
+Console.WriteLine("The difference between red and yellow apples is: " + (redApples - yellowApples));
---
+++ homeworktrain/homeworktrain/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("lenght of train (meters) = ");
+int trainLenght = int.Parse(Console.ReadLine());
+Console.Write("Speed (m/sec) = ");
+int trainSpeed = int.Parse(Console.ReadLine());
+Console.Write("time (sec) =");
+int trainTime = int.Parse(Console.ReadLine());
+
+trainTime -= trainLenght / trainSpeed;
+
+Console.WriteLine(trainSpeed * trainTime);
